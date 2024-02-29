52ca8f257e7e869433df1d96cad93452969df7cc
Adi Zafri <adizafri2000@gmail.com>
2024-02-29 23:07:38 +0800

    added request logging

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,9 +7,11 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    print(f"Request at '/' received at {datetime.now()}")
     return {"message": "Hello World"}
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
+    print(f"Request at '/hello/name' received at {datetime.now()}, name: {name}")
     return {"message": f"Hello {name}"}
