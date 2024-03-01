385666340f489a044b8c39a95595931e3586b9b5
Adi Zafri <61119610+adizafri2000@users.noreply.github.com>
2024-03-01 17:17:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    print(f"Request at '/' received at {datetime.now()}")
+    #print(f"Request at '/' received at {datetime.now()}")
     return {"message": "Hello World"}
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
-    print(f"Request at '/hello/name' received at {datetime.now()}, name: {name}")
+    #print(f"Request at '/hello/name' received at {datetime.now()}, name: {name}")
     return {"message": f"Hello {name}"}
